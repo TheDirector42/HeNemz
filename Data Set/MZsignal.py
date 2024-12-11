9c3f770647c5ef08e74c1d540b029588679cd824
Gold <keishemoo@gmail.com>
2024-12-12 09:38:55 +1000

    Updated data (tsunde fixed aligment)+ incompleate code to graph it

--- Data Set/MZsignal.py
+++ Data Set/MZsignal.py
@@ -1,4 +1,14 @@
-import numpy as np
-data = np.loadtxt('20241211_test1.dat')
+import matplotlib.pyplot as plt
+filepath= "2024-11-12-Test2-PDout.txt"
+results=[]
+with open(filepath, encoding='latin_1') as f:
+	for line in f:
+		results.append(line)
 
-print(data)
+r= results[1:]
+data=[]
+for d in r:
+	t = d.split('\t')[:-1];data.append(t)
+	
+plt.plot(results)
+plt.show()
